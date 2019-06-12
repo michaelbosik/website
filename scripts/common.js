4febaa07d542527d7e10155b8819e418e0ec2cff
mbosik <michaelbosik@gmail.com>
2019-06-11 20:56:51 -0400

    deleted unecessary content from joe integration

--- scripts/common.js
+++ scripts/common.js
@@ -1,16 +1,16 @@
-/* (c) 2019 Joseph Petitti | josephpetitti.com/license.txt */
+/* Michael Bosik */
 
 /*
  * Adds the "shown" class to the navbar, causing it to open on mobile screens
  */
 const showNav = function() {
     document.getElementById('nav').classList.add("shown");
-  }
+}
   
-  /*
-   * Removes the "shown" class from the navbar, causing it to close on mobile
-   */
-  const closeNav = function() {
+/*
+ * Removes the "shown" class from the navbar, causing it to close on mobile
+ */
+const closeNav = function() {
     document.getElementById('nav').classList.remove("shown");
-  }
+}
   
