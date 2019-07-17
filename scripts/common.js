598d7d836a18c9784c4e8ffaaf6fd2681954487a
mbosik <michaelbosik@gmail.com>
2019-07-17 09:48:52 -0400

    typewriter on homepage, last commit before revamp

--- scripts/common.js
+++ scripts/common.js
@@ -4,6 +4,11 @@
  * Show/Hide element
  */
 
+ function loadMain(){
+    typeWriter(0, "title1", "<h1>Hi, Im Michael!</h1>"); 
+    setTimeout(typeWriter, 1500, 0, "title2", "<h2>Welcome to my website!</h2>", 0);
+ }
+
  function showHide(element){
      var x = document.getElementById(element);
      if(x.style.display === "none"){
@@ -13,4 +18,10 @@
          x.style.display = "none";
      }
  }
-  
+
+ function typeWriter(i, id, txt) {
+    if (i < txt.length) {
+      document.getElementById(id).innerHTML += txt.charAt(i);
+      setTimeout(typeWriter, 50, ++i, id, txt);
+    }
+  }
