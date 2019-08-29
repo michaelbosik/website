d595dade8a0bb7e6d92200a3e030f4feab75b61d
MikeyBoss <michaelbosik@gmail.com>
2019-08-29 12:05:15 -0400

    added gompei, ireiterum, unreal, resume test, commented HTML and JS

--- scripts/common.js
+++ scripts/common.js
@@ -1,9 +1,8 @@
 /* Michael Bosik */
 
 /*
- * Show/Hide element
+ * loadMain() - initiates typewriter effect on page load
  */
-
 function loadMain() {
 	document.getElementById("title1").style.fontFamily = "Source Sans Pro";
 	document.getElementById("title2").style.fontFamily = "Source Sans Pro";
@@ -11,6 +10,9 @@ function loadMain() {
 	setTimeout(typeWriter, 1300, 0, "title2", "Welcome to my website!", 0);
 }
 
+/*
+ * typeWriter(count, id, text) - recursively called to write letter by letter to the screen
+ */
 function typeWriter(i, id, txt) {
 	if (i < txt.length) {
 		document.getElementById(id).innerHTML += txt.charAt(i);
